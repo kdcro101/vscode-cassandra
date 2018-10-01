4b9b57f49d0aed913a6c9569031d2503c78c7642
kdcro101 <kdcro101.github@gmail.com>
2018-10-01 20:29:49 +0200

    update

--- src/workspace/index.ts
+++ src/workspace/index.ts
@@ -5,7 +5,6 @@ export class Workspace {
     public getRootPath(): string {
         return vscode.workspace.workspaceFolders[0].uri.fsPath;
     }
-
     public isMultiRootWorkspace(): boolean {
         return vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 1;
     }
