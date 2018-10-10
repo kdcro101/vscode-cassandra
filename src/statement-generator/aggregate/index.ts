855c9ff654f15f1ceb269cd490f056a7d322320c
kdcro101 <kdcro101.github@gmail.com>
2018-10-10 20:50:45 +0200

    update

---
+++ src/statement-generator/aggregate/index.ts
@@ -0,0 +1,15 @@
+export const aggregateCreate = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+export const aggregateDrop = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+export const aggregateReplace = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
---
+++ src/statement-generator/type/index.ts
@@ -0,0 +1,15 @@
+export const materializedViewCreate = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+export const materializedViewDrop = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+export const materializedViewAlter = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
