0fb7e4748ee604f7cd701cf072646dca17ac127c
kdcro101 <kdcro101.github@gmail.com>
2018-10-22 18:56:05 +0200

    update

--- script/generate-parser.sh
+++ script/generate-parser.sh
@@ -1,9 +1,9 @@
-rm -rf antlr4-cql3
-echo Z2l0QGdpdGxhYi5jb206a2Rjcm8xMDEvYW50bHI0LWNxbDMuZ2l0Cg== | base64 -d | xargs git clone --quiet
-cd antlr4-cql3
-../node_modules/antlr4ts-cli/antlr4ts -Werror -visitor -listener CqlLexer.g4
-../node_modules/antlr4ts-cli/antlr4ts -Werror -visitor -listener CqlParser.g4
-cd ..
+cd res/g/cql
+../../../node_modules/antlr4ts-cli/antlr4ts -Werror -visitor -listener CqlLexer.g4
+../../../node_modules/antlr4ts-cli/antlr4ts -Werror -visitor -listener CqlParser.g4
+cd ../../..
 mkdir -p src/antlr
-mv antlr4-cql3/*.ts src/antlr
-rm -rf antlr4-cql3
+mv res/g/cql/*.ts src/antlr
+rm -rvf res/g/cql/*.tokens
+rm -rvf res/g/cql/.antlr
+echo "done"
